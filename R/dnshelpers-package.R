74ec72eed5ae8c9c14d1dce37e566b820edea553
hrbrmstr <bob@rud.is>
2018-09-08 09:02:56 -0400

    R package repo initialization complete

---
+++ R/dnshelpers-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/dnshelpers>
+#' - BugReports: <https://gitlab.com/hrbrmstr/dnshelpers/issues>
+#' 
+#' @md
+#' @name dnshelpers
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
